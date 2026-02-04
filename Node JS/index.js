9ef7b20494cadb369d9120e9b8263feebdfcf0bd
SaloniSingh2004 <saloni.pingi222@gmail.com>
2026-02-04 10:10:55 +0530

    Node

--- Node JS/index.js
+++ Node JS/index.js
@@ -2,6 +2,21 @@ const http=require('http');
 const myserver = http.createServer((req,res)=>{
     // console.log('server1');
     // res.end('Hi this is my first server');
+    if (req.url=='/'){
+        res.end("this is ABES Engineering College");
+    }
+    else if (req.url=='/about'){
+        res.end("WE are student of BTECH");
+    }
+    else if(req.url=='/contact'){
+        res.end("We are in ghaziabad")
+    }
+    else if(req.url=='/class'){
+        res.end("We are of IT A")
+    }
+    else{
+        res.end("404 page is not found");
+    }
 });
 
 myserver.listen(8000,()=>console.log('Server is run'));
